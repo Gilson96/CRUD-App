327017dd66e0c48af1ca50530c5aa5822a02f11c
Gilson96 <grafael99@gmail.com>
2020-12-18 16:26:01 +0000

    Top-Level js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TaskListContextProvider from './Context/TaskListContext';
+import { TodoList } from "./Components/TodoList/TodoList";
+import { TodoForm } from "./Components/TodoForm/TodoForm";
 
-function App() {
+import '../src/App.css';
+import { Heading } from "./Components/Heading/heading";
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TaskListContextProvider>
+      <div className="container">
+        <div className="app-wrapper">
+          <Heading />
+          <div className="main">
+            <TodoForm />
+            <TodoList />
+          </div>
+        </div>
+      </div>
+    </TaskListContextProvider>
   );
-}
+};
 
 export default App;
